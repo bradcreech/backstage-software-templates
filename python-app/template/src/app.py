9855e5f851998b31aea1484e514339ec3ae1aa81
bradcreech <tbcree4@gmail.com>
2025-10-22 07:23:34 -0500

    Updates for Lesson 97

---
+++ python-app/template/src/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+import socket
+from datetime import datetime
+app = Flask(__name__)
+
+def get_hostname():
+    return socket.gethostname()
+
+def get_date_dd_mm_yyyy():
+    return datetime.now().strftime("%d-%m-%Y")
+
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        "time": get_date_dd_mm_yyyy(),
+        "hostname": get_hostname(),
+        "message": 'You are doing a good job human! :-)',
+        "deployed_on": 'kubernetes',
+        "app_env": '${{values.app_env}}',
+        "app_name": '${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+
+def healthz():
+    return jsonify({
+        "status": "up"
+    }), 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000) 
